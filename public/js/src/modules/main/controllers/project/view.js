0af64594a7a8a369a66a731017e5891fc9e409b1
Michael V <bolsunovskiy@gmail.com>
2015-10-08 23:42:56 +0300

    implement view tabs

--- public/js/src/modules/main/controllers/project/view.js
+++ public/js/src/modules/main/controllers/project/view.js
@@ -1,5 +1,8 @@
 angular.module('main').controller('projectView',  ['$scope', 'project', '$location', 'growl', '$routeParams', function($scope, project, $location, growl, $routeParams){
 
+    $scope.viewPath = 'js/src/modules/main/views/project/view/';
+    $scope.activeTab = 'general';
+
     project.get({
         id: $routeParams.id
     }, function(data){
